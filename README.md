ecdf605918599de4da7fd7210c946cdb9a5c6e0e
Evis Drenova <evis@usenucleus.cloud>
2025-04-18 22:09:47 -0700

    add template

---
+++ README.md
@@ -0,0 +1,6 @@
+# Tauri Template with React, Typescript, Tailwind and Shadcn already installed
+
+## Installation
+
+- clone the repo
+- run `npm run tauri dev` to start the application
---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import { invoke } from "@tauri-apps/api/core";
+import "./globals.css";
+
+function App() {
+  const [greetMsg, setGreetMsg] = useState("");
+  const [name, setName] = useState("");
+
+  async function greet() {
+    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+    setGreetMsg(await invoke("greet", { name }));
+  }
+
+  return (
+    <main className="container">
+      <div className="text-3xl text-red-500">
+        Welcome to Tauri + React + Tailwind
+      </div>
+      <form
+        className="row"
+        onSubmit={(e) => {
+          e.preventDefault();
+          greet();
+        }}
+      >
+        <input
+          id="greet-input"
+          onChange={(e) => setName(e.currentTarget.value)}
+          placeholder="Enter a name..."
+        />
+        <button type="submit">Greet</button>
+      </form>
+      <p>{greetMsg}</p>
+    </main>
+  );
+}
+
+export default App;
